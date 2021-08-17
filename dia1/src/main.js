a51a9bf5c25a2e61b18f6fe0ecd91d25bc75141a
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-08-17 14:07:14 -0300

    exibir/esconder .app

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,9 +4,11 @@ document.querySelector('.app').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
-const link = document.querySelector(' [data-js="link"]')
 
-link.addEventListener('click', (event) => {
+const app = document.querySelector('[data-js = "app"]')
+const btnlink = document.querySelector('[data-js="btn"]')
+
+btnlink.addEventListener('click', (event) => {
   event.preventDefault()
-  console.log('clicou!', event.currentTarget)
-}, false)
+  app.style.display = app.style.display === "none" ? "block" : "none"
+})
