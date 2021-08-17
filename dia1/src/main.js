895a840e03917de7c266a74235cababc258464fb
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-08-17 13:52:44 -0300

    link HTML

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,9 @@ document.querySelector('.app').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const link = document.querySelector(' [data-js="link"]')
+
+link.addEventListener('click', (event) => {
+  event.preventDefault()
+  console.log('clicou!', event.currentTarget)
+}, false)
