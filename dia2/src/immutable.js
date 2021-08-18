8ee87252492c6aafcdbc66fb391583c013c78d1e
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-08-18 10:53:08 -0300

    criação objeto

--- dia2/src/immutable.js
+++ dia2/src/immutable.js
@@ -1,14 +1,18 @@
 const john = {
   name: 'John',
-  surname: 'Doe',
-  age: 30,
+  surname: 'Santos',
+  age: 35,
   hobbies: ['Surf', 'Design'],
 }
 
-const jane = john
+const jane = {
+  name: 'Jane',
+  surname: 'Silva',
+  age: '29',
+hobbies: ['dance'],
+}
 
-jane.name = 'Jane'
-jane.hobbies.push('MuayThai', 'Programming')
 
 console.log('John:', john)
 console.log('Jane:', jane)
+
