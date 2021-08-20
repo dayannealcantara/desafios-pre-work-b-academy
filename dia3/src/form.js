ddf272199739e5688c61fd9dff9f36b8cd883317
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-08-20 12:26:36 -0300

    exercicio 2

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -20,3 +20,47 @@ function fixCase (word) {
 
  return `${word[0].toUpperCase()}${word.slice(1).toLowerCase()}`
 }
+
+function $ (selector) {
+  return document.querySelector(selector)
+}
+
+const form = $('[data-js="form"]')
+const select = document.createElement('select')
+
+const colors = ['#4682B4', '#008000', '#FF1493', 'FF0000', '#FFFF00']
+const colorsContainer = document.createElement('div')
+colorsContainer.style.display = 'flex'
+
+colors.forEach(color => {
+  const option = document.createElement('option')
+  option.value = color
+  option.textContent = color
+  select.appendChild(Option)
+})
+
+select.setAttribute('multiple', '');
+form.appendChild(select)
+
+select.addEventListener('change', (e) =>{
+  colorsContainer.innerHTML = ''
+
+  Array.from(e.target.selectedOptions).forEach(option => {
+    const div = createDivColor(option.value)
+    colorsContainer.appendChild(div)
+  })
+})
+
+function createDivColor (value) {
+  const div = document.createElement('div')
+  div.style.width = '100px'
+  div.style.height = '100px'
+  div.style.background = value
+  return(div)
+}
+
+select.setAttribute('multiple', '')
+form.appendChild(select)
+document.body.appendChild(colorsContainer)
+
+console.log(select)
