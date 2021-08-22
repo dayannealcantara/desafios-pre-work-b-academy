69a05453d4073d0215a3b280f5b0154f1ce81989
Dayanne Alcantara <dayannealcantara14@gmail.com>
2021-08-21 22:47:04 -0300

    dia 4 - exercício 2

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,33 +1,106 @@
 import './style.css'
 
+const url = 'http://localhost:3333/cars'
 const form = document.querySelector('[data-js="cars-form"]')
 const table =  document.querySelector('[data-js="table"]')
 
-const getFormElemenet = ( event ) => (elementName) =>{
+const getFormElemenet = ( event ) => (elementName) => {
   console.log(event.target.elements[elementName])
   return event.target.elements[elementName]
+}
+
+const elementTypes = {
+  image: createImage,
+  text: createText,
+  color: createColor,
+}
+
+function createImage (value) {
+  const td = document.createElement('td')
+  const img = document.createElement('img')
+  img.src = value
+  img.width = 100
+  td.appendChild(img)
+  return td
+}
+
+function createText (value) {
+  const td = document.createElement('td')
+  td.textContent = value
+  return td
+}
+
+function createColor (value) {
+  const td = document.createElement('td')
+  const div = document.createElement('div')
+  div.style.width = '100px'
+  div.style.height = '100px'
+  div.style.background = value
+  td.appendChild(color)
+  return td
 }
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   const getElement = getFormElemenet(e)
-  const image = getElement('image')
-  const brandModel = getElement('brand-model')
-  const year = getElement('year')
-  const color = getElement('color')
-  const plate = getElement('plate')
 
-  const elements = [image, brandModel, year, color, plate]
+  const data = {
+    image: getElement('image').value ,
+    brandModel: getElement('brand-model').value ,
+    year: getElement('year').value ,
+    color: getElement('color').value ,
+    plate: getElement('plate').value,
+  }
+  createTableRow(data)
 
-  const tr = document.createElement('tr')
-  elements.forEach(element => {
-    const td = document.createElement('td')
-    td.textContent = element.value
-    tr.appendChild(td)
-  })
-  table.appendChild(tr)
 
   e.target.reset()
   image.focus()
 })
 
+function createTableRow (data) {
+   const elements = [
+    { type: 'image', value: data.image },
+    { type: 'text', value: data.brandModel },
+    { type: 'text', value: data.year },
+    { type: 'color', value: data.color },
+    { type: 'text', value: data.plate },
+  ]
+
+  const tr = document.createElement('tr')
+  elements.forEach(element => {
+    const td = elementTypes[element.type](element.value)
+    tr.appendChild(td)
+  })
+  table.appendChild(tr)
+}
+
+function createNoCarRow () {
+  const tr = document.createElement('tr')
+  const td = document.createElement('td')
+  const thsLength = document.querySelectorAll('table th')
+  td.setAttribute('colspan', thsLength)
+  td.textContent = 'Nenhum carro encontrado'
+  tr.appendChild(td)
+  table.appendChild(tr)
+}
+
+ async function main () {
+    const result = await fetch(url)
+      .then(r => r.json ())
+      .catch( e => ({ error : true, message: e.message }))
+
+      if (result.error) {
+        console.log('Erro ao buscar carros', result.message)
+        return
+   }
+      if (result.lenght === 0) {
+        createNoCarRow()
+        return
+      }
+
+ result.forEach(createTableRow)
+
+}
+
+ main()
